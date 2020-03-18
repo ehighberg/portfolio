87db07312f649f069ee1c52eac9af0c59da9a104
Errol Highberg <errol.highberg@gmail.com>
2020-03-18 12:55:49 -0400

    projectList with one member

---
+++ src/projectList.js
@@ -0,0 +1,13 @@
+const projectList = [
+  {
+    name: 'Grid Flows',
+    thumbUrl: '../assets/thumb-grid-flows.png',
+    projectType: 'Front End App',
+    description: 'View the flow of national electric grid generation and demand over time. Hourly -> monthly timeframe, animated and visualized with d3.js. Data courtesy of the Energy Information Administration.',
+    siteUrl: 'https://grid-flows.surge.sh/',
+    gitUrl: 'https://github.com/ehighberg/grid-flows',
+    techList: ['react', 'html', 'css', 'd3']
+  }
+]
+
+export default projectList
