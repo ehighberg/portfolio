f2c65fc238c600704308e9aab2de911eb9184c78
Errol Highberg <errol.highberg@gmail.com>
2020-03-18 13:24:03 -0400

    ProjectCard skeleton, assets moved to public

--- src/component/ProjectCard.jsx
+++ src/component/ProjectCard.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
 const ProjectCard = props => {
+  const { name, thumbUrl, projectType, description,
+    siteUrl, gitUrl, techList } = props.project
+
   return (
     <article>
-
+      <img alt={`project ${name} thumbnail`} src={thumbUrl} />
+      <section>
+        <h1><a href={siteUrl}>{name}</a></h1>
+        <h3>{projectType}</h3>
+        <p className='project-card-description'>{description}</p>
+        <a href={siteUrl}>Website</a>
+        <a href={gitUrl}>Github</a>
+        <p>Technologies used</p>
+        <ul>
+          {techList.map((tech, i) => (
+            <li key={i}><p className='project-tech-img'>{tech}</p></li>
+          ))}
+        </ul>
+      </section>
     </article>
   )
 }
--- src/projectList.js
+++ src/projectList.js
@@ -1,7 +1,7 @@
 const projectList = [
   {
     name: 'Grid Flows',
-    thumbUrl: '../assets/thumb-grid-flows.png',
+    thumbUrl: '/assets/thumb-grid-flows.png',
     projectType: 'Front End App',
     description: 'View the flow of national electric grid generation and demand over time. Hourly -> monthly timeframe, animated and visualized with d3.js. Data courtesy of the Energy Information Administration.',
     siteUrl: 'https://grid-flows.surge.sh/',
