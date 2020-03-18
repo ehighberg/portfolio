4ffb29b2e5767ce22c38e8e121cd628283e08eea
Errol Highberg <errol.highberg@gmail.com>
2020-03-18 12:03:59 -0400

    Nav skeleton

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Nav = props => {
   return (
     <nav>
-
+      <div className='nav-name'>
+        <a href='#home'>Errol Highberg</a>
+      </div>
+      <div className='nav-items'>
+        <a href='#projects'>Projects</a>
+        <a href='#about'>About</a>
+        <a href='#contact'>Contact</a>
+      </div>
     </nav>
   )
 }
