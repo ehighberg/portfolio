4f6c7cbad7a086adb9c108386a39305130bb1c3a
Errol Highberg <errol.highberg@gmail.com>
2020-03-18 12:59:31 -0400

    Projects skeleton

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import ProjectCard from './ProjectCard'
+import projectList from '../projectList'
 
 const Projects = props => {
   return (
     <main>
-
+      <h1>Projects</h1>
+      {projectList.map((project, i) =>
+        <ProjectCard project={project} key={i} />
+      )}
     </main>
   )
 }
