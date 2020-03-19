b2e4fa6c888c3b76a4cda359fc69d20fbbe03e9a
Errol Highberg <errol.highberg@gmail.com>
2020-03-19 18:37:18 -0400

    working contact form

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,35 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import '../style/contact.css'
 
 const Contact = React.forwardRef((props, ref) => {
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
   return (
     <footer>
       <h1>Contact</h1>
       <p>Interested in collaborating?</p>
       <p>Send me an email!</p>
-      <h1>FORM</h1>
+      <form
+        action="https://formspree.io/mwkqnknp"
+        method="POST"
+      >
+        <input
+          type="text"
+          name="_replyto"
+          value={email}
+          placeholder='Your Email'
+          onChange={(elem) => setEmail(elem.target.value)}
+        />
+        <textarea
+          name="message"
+          value={message}
+          placeholder='Your Message'
+          onChange={(elem) => setMessage(elem.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
     </footer>
   )
 })
 
 export default Contact
-
-
-// Code generated in part by formspree.io
-
-// <form
-//   action="https://formspree.io/mwkqnknp"
-//   method="POST"
-// >
-//   <label>
-//     Your email:
-//     <input type="text" name="_replyto">
-//   </label>
-//   <label>
-//     Your message:
-//     <textarea name="message"></textarea>
-//   </label>
-//
-//   <!-- your other form fields go here -->
-//
-//   <button type="submit">Send</button>
-// </form>
