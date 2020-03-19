49d125888a10f9a930ef5e572d51a453ad4f75e1
Errol Highberg <errol.highberg@gmail.com>
2020-03-19 17:51:22 -0400

    about top styled

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import '../style/about.css'
 
 const About = React.forwardRef((props, ref) => {
   return (
     <summary>
       <h1>About Me</h1>
-      <p className='about-main'>Lorem Ipsum Dolor</p>
-      <p className='about-zinger'>Lorem Ipsum Dolor</p>
+      <p className='about-main'>Full-stack developer and data scientist, driven to understand systems and their interactions. Reinforced by attention to detail and determination, I welcome and conquer challenges in any domain.</p>
+      <br />
+      <p className='about-zinger'>Ask me about high-concept science-fiction or non-Newtonian calculus.</p>
       <ul className='about-links'>
         <li><a target="_blank" rel="noopener noreferrer" href='RESUME'>Resume</a></li>
         <li><a target="_blank" rel="noopener noreferrer" href='LINKEDIN'>LinkedIn</a></li>
