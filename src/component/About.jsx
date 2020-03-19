563bbb8ed55fee63384c5bca5a168f6342cb4fae
Errol Highberg <errol.highberg@gmail.com>
2020-03-19 18:14:48 -0400

    about styled

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -8,11 +8,17 @@ const About = React.forwardRef((props, ref) => {
       <p className='about-main'>Full-stack developer and data scientist, driven to understand systems and their interactions. Reinforced by attention to detail and determination, I welcome and conquer challenges in any domain.</p>
       <br />
       <p className='about-zinger'>Ask me about high-concept science-fiction or non-Newtonian calculus.</p>
-      <ul className='about-links'>
-        <li><a target="_blank" rel="noopener noreferrer" href='RESUME'>Resume</a></li>
-        <li><a target="_blank" rel="noopener noreferrer" href='LINKEDIN'>LinkedIn</a></li>
-        <li><a target="_blank" rel="noopener noreferrer" href='GITHUB'>Github</a></li>
-      </ul>
+      <div className='about-links'>
+        <a target="_blank" rel="noopener noreferrer" href='RESUME'>
+          <img className='about-icon' alt='resume-icon' src='https://s3.amazonaws.com/image-urls/images/resume-icon-16.jpg' />
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href='https://linkedin.com/in/errol-highberg'>
+          <img className='about-icon' alt='linkedin-icon' src='https://s3.amazonaws.com/image-urls/images/square-linkedin-512.png' />
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href='https://github.com/ehighberg'>
+          <img className='about-icon' alt='github-icon' src='https://s3.amazonaws.com/image-urls/images/githubicon.png' />
+        </a>
+      </div>
     </summary>
   )
 })
