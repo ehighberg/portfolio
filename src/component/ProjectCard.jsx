cb6760a4a69fa2fe0630daa8706aa89bd7bdf679
Errol Highberg <errol.highberg@gmail.com>
2020-06-15 14:30:01 -0400

    added fetch and kanary projects

--- src/component/ProjectCard.jsx
+++ src/component/ProjectCard.jsx
@@ -15,7 +15,9 @@ const ProjectCard = props => {
     python: 'https://www.python.org/static/opengraph-icon-200x200.png',
     keras: 'https://upload.wikimedia.org/wikipedia/commons/c/c9/Keras_Logo.jpg',
     sklearn: 'https://www.stat4decision.com/wp-content/uploads/2017/09/scikit-learn.png',
-    numpy: 'https://user-images.githubusercontent.com/98330/63813335-20cd4b80-c8e2-11e9-9c04-e4dbf7285aa1.png'
+    numpy: 'https://user-images.githubusercontent.com/98330/63813335-20cd4b80-c8e2-11e9-9c04-e4dbf7285aa1.png',
+    django: 'http://icon-library.com/images/django-icon/django-icon-0.jpg',
+    celery: 'https://cdn.imgbin.com/3/22/11/imgbin-celery-python-node-js-task-celery-kP1fb5f1qcX0BFDbhueH7e9zJ.jpg'
   }
 
   const fetchImgUrl = tech => techUrls[tech]
@@ -35,13 +37,13 @@ const ProjectCard = props => {
         <p className='project-card-description'>{description}</p>
         <div className='project-card-links'>
           <a target="_blank" rel="noopener noreferrer" href={siteUrl}>Website</a>
-          <a target="_blank" rel="noopener noreferrer" href={gitUrl}>Github</a>
+          <a target="_blank" rel="noopener noreferrer" href={gitUrl}>{gitUrl ? 'Github' : ''}</a>
         </div>
         <div className='project-card-tech-container'>
           <p>Technologies used</p>
           <ul className='project-card-tech-list'>
             {techList.map((tech, i) => (
-              <li key={i}><img className='project-card-tech-img' alt='tech-used' src={fetchImgUrl(tech)}/></li>
+              <li key={i}><img className='project-card-tech-img' alt='tech used' src={fetchImgUrl(tech)}/></li>
             ))}
           </ul>
         </div>
--- src/projectList.js
+++ src/projectList.js
@@ -1,8 +1,26 @@
 const projectList = [
   {
+    name: 'The Kanary',
+    thumbUrl: '/assets/thumb-kanary.png',
+    projectType: 'Full Stack Contract',
+    description: 'The Kanary is a service for identifying your personal information on the public web, and assisting you with removing that information. My contribution is the implementation of the SocialScan API for finding your information on social media platforms, using Django and Celery.',
+    siteUrl: 'https://thekanary.com',
+    gitUrl: '',
+    techList: ['python', 'django', 'celery']
+  },
+  {
+    name: 'Fetch',
+    thumbUrl: '/assets/thumb-fetch.png',
+    projectType: 'Full Stack App',
+    description: 'The winning entry to the GAxDatadog Hackathon 2020, Fetch is a collaborative project of 5 software developers and user experience designers, which serves as a company directory with social media features for companies working remotely. I was largely responsible for application logic in JavaScript and Ruby, as well as the project documentation.',
+    siteUrl: 'https://fetch-hs.netlify.app/',
+    gitUrl: 'https://github.com/ehighberg/fetch',
+    techList: ['react', 'ruby', 'js']
+  },
+  {
     name: 'Terrarium',
     thumbUrl: '/assets/thumb-terrarium.png',
-    projectType: 'Full-stack / Data Science App',
+    projectType: 'Full stack / Data Science App',
     description: 'Terrarium is a web interface for running, tracking, and managing machine learning (ML) experiments. Served by Ruby on Rails and ML performed with Python and NumPy.',
     siteUrl: 'https://terrarium-ml.surge.sh/',
     gitUrl: 'https://github.com/ehighberg/terrarium',
