b129ef3a2f12c221c545963abd1efb1154a2d086
Errol Highberg <errol.highberg@gmail.com>
2020-06-16 08:59:11 -0400

    images shrink to left

--- src/component/ProjectCard.jsx
+++ src/component/ProjectCard.jsx
@@ -16,7 +16,7 @@ const ProjectCard = props => {
     keras: 'https://upload.wikimedia.org/wikipedia/commons/c/c9/Keras_Logo.jpg',
     sklearn: 'https://www.stat4decision.com/wp-content/uploads/2017/09/scikit-learn.png',
     numpy: 'https://user-images.githubusercontent.com/98330/63813335-20cd4b80-c8e2-11e9-9c04-e4dbf7285aa1.png',
-    django: 'http://icon-library.com/images/django-icon/django-icon-0.jpg',
+    django: 'https://icon-library.com/images/django-icon/django-icon-0.jpg',
     celery: 'https://cdn.imgbin.com/3/22/11/imgbin-celery-python-node-js-task-celery-kP1fb5f1qcX0BFDbhueH7e9zJ.jpg'
   }
 
